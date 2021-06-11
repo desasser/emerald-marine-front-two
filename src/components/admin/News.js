b7bdcbd918efc587e0f14618167a1df9ea76726b
shame-wizards-apprentice <rachelnelsonschille@gmail.com>
2021-06-11 14:50:51 -0700

    data-fetching parts of admin dashboard completed

--- src/components/admin/News.js
+++ src/components/admin/News.js
@@ -1,7 +1,8 @@
 import React, {useState} from "react";
 import API from '../../utils/API';
+import store from '../../utils/store';
 
-const News = (props) => {
+const News = () => {
     return (
         <form>
         <h1>Here's a news form, dummy</h1>
