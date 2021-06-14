35fa60fa835a6466d32aae77484f130359efc197
shame-wizards-apprentice <rachelnelsonschille@gmail.com>
2021-06-14 09:13:22 -0700

    added view functionality for news articles

--- src/components/admin/News.js
+++ src/components/admin/News.js
@@ -1,12 +1,26 @@
-import React, {useState} from "react";
-import API from '../../utils/API';
+import React from "react";
+import Grid from '@material-ui/core/Grid';
+import BlogCard from '../BlogCard';
 import store from '../../utils/store';
 
 const News = () => {
+    const articles = store.getState().newsArticles.newsArticles
+
     return (
-        <form>
-        <h1>Here's a news form, dummy</h1>
-        </form>
+        <div>
+             <Grid container spacing={2}>
+                <Grid item xs={12}>
+                    <h1>Current News Articles</h1>
+                    <br/>
+                </Grid>
+                <Grid container spacing={3} justify='space-evenly'>
+                    {articles.map(article => 
+                        <Grid item xs={6}>
+                            <BlogCard id='#' title={article.title} image={'http://placekitten.com/g/200/300'} alt={'not a cat'} children={article.publication}/>
+                        </Grid>)}
+                </Grid>
+            </Grid>
+        </div>
     )
     
 }
