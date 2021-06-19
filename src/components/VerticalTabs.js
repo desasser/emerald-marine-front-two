18234c09493c8cdc142267757f6723627cd92e48
Daniel Sasser <desasser79@gmail.com>
2021-06-18 22:08:24 -0700

    pulling dev

--- src/components/VerticalTabs.js
+++ src/components/VerticalTabs.js
@@ -69,10 +69,6 @@ export default function VerticalTabs({ content }) {
         aria-label="Vertical tabs for marketing info"
         className={classes.tabs}
       >
-        {/* <Tab label="Alerts in seconds" {...a11yProps(0)} />
-        <Tab label="Automatically activated" {...a11yProps(1)} />
-        <Tab label="Wired for engine kill" {...a11yProps(2)} />
-        <Tab label="Plots overboard waypoint" {...a11yProps(3)} /> */}
         {content.map(e => (
           <Tab label={e.title} {...a11yProps(e.id)} />
         ))}
@@ -82,33 +78,6 @@ export default function VerticalTabs({ content }) {
           {e.content}
         </TabPanel>
       ))}
-
-      {/* <TabPanel value={value} index={0}>
-        <p>
-          When every second counts, the ALERT system instantaneously alarms the vessel, providing the crew the critical time needed for a successful man-overboard rescue. Unlike AIS, the response of the ALERT System is immediate.
-        </p>
-        <p>
-          There is no lag time connecting with satellites, and there is no reliance on another product to turn on our automatic water-activated alerting device.
-        </p>
-      </TabPanel>
-      <TabPanel value={value} index={1}>
-        <p>
-          The lightweight, water-activated Transmitter easily attaches to a personal flotation device. Upon immersion, the Transmitter instantly alerts the crew of a man-overboard.
-        </p>
-        <p>
-          The proprietary 418Mhz frequency allows it to be automatically activated so it doesn’t have to rely on additional equipment to be triggered.
-        </p>
-      </TabPanel>
-      <TabPanel value={value} index={2}>
-        <p>
-          For Singlehanders and Solo Fisherman, the ALERT System can be wired to the boat engine. In the event that someone falls overboard, the ALERT System can shut down the engine, giving the man-overboard the opportunity to get back to the vessel.
-        </p>
-      </TabPanel>
-      <TabPanel value={value} index={3}>
-        <p>
-          The ALERT Receiver can be connected to a compatible GPS plotter to instantly mark the boat’s location at the time of the fall overboard. This makes for a speedy and successful water rescue.
-        </p>
-      </TabPanel> */}
     </div>
   )
 }
