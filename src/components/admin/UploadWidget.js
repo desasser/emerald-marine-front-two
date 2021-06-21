2e1f3ab815def8e9ac578c25bf79ed66484454e1
shame-wizards-apprentice <rachelnelsonschille@gmail.com>
2021-06-21 12:18:41 -0700

    styled upload button to match material ui

--- src/components/admin/UploadWidget.js
+++ src/components/admin/UploadWidget.js
@@ -25,14 +25,15 @@ const UploadWidget = () => {
             resourceType={'image'}
             uploadPreset={'txcgfxda'}
             cloudName={'dccnlakey'}
-            buttonText={'Upload Photo'}
+            buttonText={'UPLOAD PHOTO'}
             style={{
-                color: 'white',
+                color: 'black',
                 border: 'none',
-                width: '120px',
-                backgroundColor: 'green',
+                width: '168px',
+                backgroundColor: '#e0e0e0',
                 borderRadius: '4px',
-                height: '25px'
+                height: '25px',
+                boxShadow: '0px 3px 1px -2px rgb(0 0 0 / 20%), 0px 2px 2px 0px rgb(0 0 0 / 14%), 0px 1px 5px 0px rgb(0 0 0 / 12%)'
               }}
               folder={'Emerald Marine Images'}
               use_filename={true}
