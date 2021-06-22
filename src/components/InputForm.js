f5c608655b9a5b86832b98a01040a7275d0e8432
Daniel Sasser <desasser79@gmail.com>
2021-06-22 11:44:44 -0700

    adding email submission function

--- src/components/InputForm.js
+++ src/components/InputForm.js
@@ -5,11 +5,15 @@ import { TextField, Button, Typography } from '@material-ui/core';
 
 export default function InputForm({ buttonText, classes, label, text }) {
 
+  const submitEmail = () => {
+    console.log('email submitted')
+  }
+
   return (
     <form className={classes.formStyle}>
       <Typography style={{ textAlign: 'center', marginBottom: '0px' }}>{text}</Typography>
       <TextField className={classes.inputStyle} id="outlined-basic" label={label} variant="outlined" />
-      <Button variant="contained" style={{ marginTop: '10px', height: '56px', marginLeft: '10px', width: '100px', backgroundColor: '#f5ed5eff', fontSize: '16px' }}>{buttonText}</Button>
+      <Button variant="contained" style={{ marginTop: '10px', height: '56px', marginLeft: '10px', width: '100px', backgroundColor: '#74b4ab', fontSize: '16px' }} onClick={submitEmail} >{buttonText}</Button>
     </form>
   )
 }
