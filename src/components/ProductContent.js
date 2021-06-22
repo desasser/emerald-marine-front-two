e0f6bd8d622e942eafeafe6d387b91ca92862e43
Daniel Sasser <desasser79@gmail.com>
2021-06-22 11:16:58 -0700

    product page checkboxes setup

--- src/components/ProductContent.js
+++ src/components/ProductContent.js
@@ -1,9 +1,10 @@
-import React from 'react'
-import {useSelector} from 'react-redux';
+import React, { useState } from 'react'
+import { useSelector } from 'react-redux';
 import ProductCard from '../components/ProductCard'
 import InputForm from '../components/InputForm'
 import { Container, Grid, Typography } from '@material-ui/core'
 import { makeStyles } from '@material-ui/core/styles';
+import Checkbox from '@material-ui/core/Checkbox';
 
 const useStyles = makeStyles((theme) => ({
   flexBox: {
@@ -44,14 +45,50 @@ const useStyles = makeStyles((theme) => ({
 export default function ProductContent() {
   const classes = useStyles();
   const products = useSelector(state => state.products.products)
+  const [state, setState] = useState({
+    checkedA: true,
+    checkedB: true,
+    checkedC: true
+  });
+
+  const handleChange = (event) => {
+    setState({ ...state, [event.target.name]: event.target.checked });
+  };
+
+  const categories = ['Man-Overboard', 'Marine Safety', 'Water Rescue Training']
 
   return (
     <Container style={{ marginTop: '1.5em' }}>
-      <div style={{display:'flex', alignItems:'flex-end', width: '100%', justifyContent: 'space-between'}}>
+      <div style={{ display: 'flex', alignItems: 'flex-end', width: '100%', justifyContent: 'space-between' }}>
         <Typography variant='h2' style={{ marginTop: '50px', color: '#74b4ab', display: 'inline-block' }}>All Products</Typography>
-        <InputForm classes={classes} text={''} label={'search...'} buttonText={'search'} />
+        <div>
+          <Checkbox
+            checked={state.checkedA}
+            onChange={handleChange}
+            color="default"
+            name="checkedA"
+            inputProps={{ 'aria-label': `Man-Overboard checkbox` }}
+          />
+          <Typography component="span">Man-Overboard</Typography>
+          <Checkbox
+            checked={state.checkedB}
+            onChange={handleChange}
+            color="default"
+            name="checkedB"
+            inputProps={{ 'aria-label': `Marine Safety checkbox` }}
+          />
+          <Typography component="span">Marine Safety</Typography>
+          <Checkbox
+            checked={state.checkedC}
+            onChange={handleChange}
+            color="default"
+            name="checkedC"
+            inputProps={{ 'aria-label': `Water Rescue Training checkbox` }}
+          />
+          <Typography component="span">Water Rescue Training</Typography>
+        </div>
       </div>
-      <hr style={{marginBottom: '2em'}}></hr>
+      <hr style={{ marginBottom: '2em' }}></hr>
       <Grid container className={classes.flexBox} spacing={2}>
         <Grid item xs={12}>
           <Grid container justify='center' spacing={4}>
