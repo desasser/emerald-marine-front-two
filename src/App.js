1aa2feac9779fda4ec7cfd0431836ac9c71724f0
Daniel Sasser <desasser79@gmail.com>
2021-06-25 12:02:32 -0700

    fixing redirect before current product or blog loads bug

--- src/App.js
+++ src/App.js
@@ -1,7 +1,7 @@
 import React from 'react';
 import Header from './components/Header'
 import Footer from './components/Footer'
-import { BrowserRouter as Router, Switch, Route } from "react-router-dom";
+import { BrowserRouter as Router, Switch, Route, Redirect } from "react-router-dom";
 import { makeStyles, CssBaseline } from '@material-ui/core';
 import { ThemeProvider } from '@material-ui/core/styles'
 import Cart from "./pages/Cart"
@@ -17,6 +17,7 @@ import SingleProduct from "./pages/SingleProduct"
 import SingleBlog from "./pages/SingleBlog"
 import Support from "./pages/Support"
 import theme from './theme-provider'
+import { useSelector } from 'react-redux';
 
 const useStyles = makeStyles({
   main: {
@@ -32,6 +33,12 @@ const useStyles = makeStyles({
 
 function App() {
   const classes = useStyles()
+  const products = useSelector(state => state.products.products)
+  const blog = useSelector(state => state.blog.blog)
+  const pr = useSelector(state => state.pressReleases.pressReleases)
+  const news = useSelector(state => state.newsArticles.newsArticles)
+
+  const allPosts = blog.concat(pr, news);
 
   return (
     <ThemeProvider theme={theme}>
@@ -45,9 +52,13 @@ function App() {
             <Route path="/contact" component={Contact} />
             <Route path="/home" component={Home} />
             <Route path="/oscarwaterrescue" component={Oscar} />
-            <Route path="/news/:id" component={SingleBlog} />
+            <Route path="/news/:id">
+              {allPosts.length === 0 ? <Redirect to="/products" /> : <SingleBlog/>}
+            </Route>
             <Route path="/news" component={News} />
-            <Route path="/product/:sku" component={SingleProduct} />
+            <Route path="/product/:sku">
+              {products.length === 0 ? <Redirect to="/products" /> : <SingleProduct/>}
+            </Route>
             <Route path="/products" component={Products} />
             <Route path="/support" component={Support} />
             <Route path='/login' component={AdminLogin} />
