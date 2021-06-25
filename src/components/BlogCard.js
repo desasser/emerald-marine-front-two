9e6216f386a4ca398adcf210a997339ebabae04b
Daniel Sasser <desasser79@gmail.com>
2021-06-25 12:33:10 -0700

    links versus a-tags now work for navigating blog/news/PR articles

--- src/components/BlogCard.js
+++ src/components/BlogCard.js
@@ -15,28 +15,35 @@ const useStyles = makeStyles({
   },
 });
 
-export default function BlogCard({ children, id, title, image, alt, date, categories, tags, intro, content, publication, link, description, removeMe, grabMe, view, type }) {
+export default function BlogCard({ id, title, image, alt, date, categories, tags, intro, content, publication, link, description, removeMe, grabMe, view, type }) {
   const classes = useStyles();
 
   return (
     <Card className={classes.root}>
-      <Link to={`/news/${id}`} style={{textDecoration: 'none', color: 'black'}}>
-        <CardActionArea >
+      {!link?
+      
+      <Link to={`/news/${id}`} style={{ textDecoration: 'none', color: 'black' }}>
+      <CardActionArea >
           <CardMedia
             className={classes.media}
             image={image ? `${image}` : `${stockPressPhoto}`}
             title={`${alt}`}
           />
           <CardContent>
-            <Typography gutterBottom variant="h5" component="h2">
+            <Typography gutterBottom variant="h5" component="h2" style={{ color: '#74b4ab' }}>
               {title}
-            </Typography>
-            <Typography gutterBottom variant="body1" component="h2" style={{ textAlign: 'right' }}>
-              {date}
             </Typography>
             <Typography variant="body2" color="textSecondary" component="p">
               {intro}
             </Typography>
+            <div style={{ display: 'flex', width: '100%', justifyContent: 'space-between' }}>
+              <Typography gutterBottom variant="body1" component="h2" style={{ textAlign: 'right', fontStyle: 'italic' }}>
+                {publication}
+              </Typography>
+              <Typography gutterBottom variant="body1" component="h2" style={{ textAlign: 'right' }}>
+                {date}
+              </Typography>
+            </div>
             <Typography variant="body1" color="textSecondary" component="p" style={{ textAlign: 'right', paddingTop: '5px', color: 'black' }}>
             </Typography>
             {view === 'admin' ?
@@ -47,6 +54,40 @@ export default function BlogCard({ children, id, title, image, alt, date, catego
           </CardContent>
         </CardActionArea>
       </Link>
+      :
+      <a href={link} style={{ textDecoration: 'none', color: 'black' }} target="_blank">
+        <CardActionArea >
+          <CardMedia
+            className={classes.media}
+            image={image ? `${image}` : `${stockPressPhoto}`}
+            title={`${alt}`}
+          />
+          <CardContent>
+            <Typography gutterBottom variant="h5" component="h2" style={{ color: '#74b4ab' }}>
+              {title}
+            </Typography>
+            <Typography variant="body2" color="textSecondary" component="p">
+              {intro}
+            </Typography>
+            <div style={{ display: 'flex', width: '100%', justifyContent: 'space-between' }}>
+              <Typography gutterBottom variant="body1" component="h2" style={{ textAlign: 'right', fontStyle: 'italic' }}>
+                {publication}
+              </Typography>
+              <Typography gutterBottom variant="body1" component="h2" style={{ textAlign: 'right' }}>
+                {date}
+              </Typography>
+            </div>
+            <Typography variant="body1" color="textSecondary" component="p" style={{ textAlign: 'right', paddingTop: '5px', color: 'black' }}>
+            </Typography>
+            {view === 'admin' ?
+              <div>
+                <Button size="small" data-id={id} data-title={title} data-image={image} data-alt={alt} data-description={description} data-tags={tags} data-categories={categories} data-date={date} data-intro={intro} data-content={content} data-publication={publication} data-link={link} onClick={removeMe}>Remove {type} </Button>
+                <Button size="small" data-id={id} data-title={title} data-image={image} data-alt={alt} data-description={description} data-tags={tags} data-categories={categories} data-date={date} data-intro={intro} data-content={content} data-publication={publication} data-link={link} onClick={grabMe}>Update {type}</Button>
+              </div> : <div></div>}
+          </CardContent>
+        </CardActionArea>
+      </a>
+    }
     </Card>
   );
 }
